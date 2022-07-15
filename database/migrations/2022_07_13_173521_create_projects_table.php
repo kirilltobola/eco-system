e64dcb8448c4f7e0784dce2fa8062ec9ada62453
Kirill Tobola <kirilltobola@gmail.com>
2022-07-15 15:18:30 +0800

    Add database migrations

---
+++ database/migrations/2022_07_13_173521_create_projects_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('projects', function (Blueprint $table) {
+            $table->id();
+            $table->boolean('published')->default(false);
+            $table->string('headline');
+            $table->text('details');
+            $table->text('realization_description')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('projects');
+    }
+};
---
+++ database/migrations/2022_07_13_175817_create_department_project_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('department_project', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('department_id')->unsigned();
+            $table->bigInteger('project_id')->unsigned();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('department_project');
+    }
+};
