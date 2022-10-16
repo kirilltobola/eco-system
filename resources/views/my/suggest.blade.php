10620caa531af3281736bbe829e8be539d9e6089
Kirill Tobola <kirilltobola@gmail.com>
2022-10-17 01:13:52 +0800

    fix margin top

--- resources/views/my/suggest.blade.php
+++ resources/views/my/suggest.blade.php
@@ -7,7 +7,7 @@
 @section('content')
     <div class="row">
         <div class="col-sm-6">
-            <div class="card">
+            <div class="card mt-2">
                 <img src="https://cdn.ttgtmedia.com/rms/onlineimages/erp-kaizen_cycle_mobile.png" class="card-img-top" alt="...">
                 <div class="card-body">
                     <h5 class="card-title">Кайзен</h5>
@@ -36,7 +36,7 @@
             </div>
         </div>
         <div class="col-sm-6">
-            <div class="card">
+            <div class="card mt-2">
                 <img src="https://i.pinimg.com/originals/d9/76/50/d9765063d67573ad5877bffe56a1e68f.png" class="card-img-top" alt="...">
                 <div class="card-body">
                     <h5 class="card-title">Проект</h5>
